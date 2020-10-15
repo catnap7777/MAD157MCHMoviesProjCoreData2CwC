189a1760963755ec136fb727cf7d531889896d13
km <35552602+catnap7777@users.noreply.github.com>
2020-10-15 09:34:13 -0500

    added MyMovieTable custom class

---
+++ MAD157MCHMoviesProjCoreData2CwC/Model/MyMovieTable+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyMovieTable+CoreDataClass.swift
+//  MAD157MCHMoviesProjCoreData2CwC
+//
+//  Created by Karen Mathes on 10/15/20.
+//  Copyright © 2020 TygerMatrix Software. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MyMovieTable)
+public class MyMovieTable: NSManagedObject {
+
+}
